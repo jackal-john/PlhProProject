15a9b4e58175dc0fac15d1c391276a78ff5cee49
jackal-john <jackal-john@outlook.com>
2022-04-28 21:45:23 +0300

    Final

--- project_plipro.py
+++ project_plipro.py
@@ -1,7 +1,11 @@
-from tkinter import *
+import tkinter as tk
 import random
 import time
 import matplotlib.pyplot as plt
+#Βιβλιοθήκη για να εμφανίσω το γράφημα μέσα στο παράθυρο
+#του tkinder
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 
 
@@ -9,12 +13,25 @@ import matplotlib.pyplot as plt
 #Παίρνει ως είσοδο μια πλειάδα t
 #και επιστρέφει την τιμή της συνάρτησης f(t0, t1, t2)
 # To x**2 σημαίνει x εις την δευτέρα.
-def  objective_function(t):
+def  standard_function(t):
     return  t[0]**2 + t[1]**3 + t[2]**4 + t[0]*t[1]*t[2]
-    #Μπορούμε να δοκιμάσουμε και άλλες συνάρτησεις.
-    #return  (t[0] - 5)**2  +  (t[1] - 10)**2  + (t[2]- 15)**2
+#--------------------------------------
+
+
+#=====================================
+#Παίρνει ως είσοδο μια πλειάδα t
+#και επιστρέφει την τιμή της συνάρτησης f(t0, t1, t2)
+def  function1(t):
+    return  (t[0] - 5)**2  +  (t[1] - 10)**2  + (t[2]- 15)**2
 #--------------------------------------
     
+
+#=====================================
+#Παίρνει ως είσοδο μια πλειάδα t
+#και επιστρέφει την τιμή της συνάρτησης f(t0, t1, t2)
+def  function2(t):
+    return  t[0]**2 - t[1]**3 + t[2]**4 - t[0]*t[1]*t[2]
+#--------------------------------------
 
 
 #=======================================
@@ -107,7 +124,7 @@ def  crossbreed(t, s):
 #Το a και το b περιγράφουν τα άκρα των πεδίων των μεταβλητών.
 #printflag: Αν εδώ βάλουμε την τιμή True τότε το πρόγραμμα θα τυπώνει σε κάθε επανάληψη την καλύτερη λύση.
 #Επιστρέφει:
-def genetic_algorithm(N, K, steps, input_type, p_m=0.3, a=[0, 0, 0], b=[10, 20, 30], printflag=False):
+def genetic_algorithm(N, K, steps, input_type, p_m=0.3, a=[0, 0, 0], b=[10, 20, 30], objective_function=standard_function, printflag=False):
     #Αρχικοποιώ τις λύσεις
     #To παρακάτω loop δημιουργεί μια λίστα (population) από 
     #N tuples.
@@ -131,7 +148,7 @@ def genetic_algorithm(N, K, steps, input_type, p_m=0.3, a=[0, 0, 0], b=[10, 20, 
     #π.χ.
     #{(1.2,  3.4,   5.1) :  25.12,
     # (2.4,  4.12,  3.5) :  24.12}
-    #
+    
     dictionary = {}
     for atom in population:
         value = objective_function(atom)
@@ -173,21 +190,23 @@ def genetic_algorithm(N, K, steps, input_type, p_m=0.3, a=[0, 0, 0], b=[10, 20, 
                 dictionary[new_solution2] = value2
             items = items + 1
         dictionary = dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True)[:K])
-        value_over_time.append(   max(dictionary.values())  )
+        #η λίστα value_over_time περιέχει τις μέγιστες τιμές από 
+        #τα values του λεξικού σε κάθε επανάληψη του γενετικού αλγορίθμου
+        value_over_time.append(  max(dictionary.values())  )
         if printflag == True:
             #Τυπώνω την καλύτερη λύση.
             s = list(dictionary)[0]
             print(n, ' ', s)
-            
-    figure = plt.figure()
-    ax = figure.add_subplot(111)
-    ax.plot(value_over_time)
+    
     #Τέλος Επανάληψης Γεννετικού 
     #----------------------------------------------
-    return dictionary
-    
+    return dictionary, value_over_time
+        
 
-    
+
+
+
+
 
 #Επιστρέφει True αν το string x είναι ακέραιος
 def   is_integer(x):
@@ -242,8 +261,8 @@ def   is_prob(x):
 #tkinter function
 def  run_genetic():
     #Καθαρίζω τα προηγούμενα κείμενα αν υπάρχουν.
-    result_outpout.delete(0.0,END)
-    solutions_outpout.delete(0.0,END)
+    result_outpout.delete(0.0,tk.END)
+    solutions_outpout.delete(0.0,tk.END)
     
     #ελέγχω αν οι τιμές είναι σωστές
     N_entered = N_entry.get()
@@ -251,7 +270,7 @@ def  run_genetic():
         N = int(N_entered)
     else:
         msg = 'Λάθος τιμή στο πλήθος αρχικών λύσεων'
-        result_outpout.insert(END,msg)
+        result_outpout.insert(tk.END,msg)
         return
     
     K_entered = K_entry.get()
@@ -259,7 +278,7 @@ def  run_genetic():
         K = int(K_entered)
     else:
         msg = 'Λάθος τιμή στο πλήθος λύσεων επόμενης γενιάς'
-        result_outpout.insert(END,msg)
+        result_outpout.insert(tk.END,msg)
         return
     
     steps_entered = steps_entry.get()
@@ -267,7 +286,7 @@ def  run_genetic():
         steps = int(steps_entered)
     else:
         msg = 'Λάθος τιμή στο πλήθος βημάτων'
-        result_outpout.insert(END,msg)
+        result_outpout.insert(tk.END,msg)
         return
     
     type_entered = type_entry.get()
@@ -275,7 +294,7 @@ def  run_genetic():
         input_type = int(type_entered)
     else:
         msg = 'Λάθος τιμή στον τύπο λύσεων'
-        result_outpout.insert(END,msg)
+        result_outpout.insert(tk.END,msg)
         return
     
     pm_entered = pm_entry.get()
@@ -283,19 +302,27 @@ def  run_genetic():
         p_m = float(pm_entered)
     else:
         msg = 'Λάθος τιμή στην πιθανότητα μετάλλαξης'
-        result_outpout.insert(END,msg)
+        result_outpout.insert(tk.END,msg)
         return
     
+    #Διαβάζω τη συνάρτηση που έχει επιλέξει ο χρήστης
+    #στο dropdown menu
+    selected_function = variable.get()
+    if selected_function == 'Standard':
+        my_function = standard_function
+    elif selected_function == 'Function1':
+        my_function = function1
+    else: my_function = function2
     
     #Τρέχω τον γενετικό αλγόριθμο
     msg = 'Ο Γεννετικός Αλγόριθμος Ξεκίνησε'
-    result_outpout.insert(END,msg)
+    result_outpout.insert(tk.END,msg)
     start = time.time()
-    solutions = genetic_algorithm(N, K, steps, input_type, p_m, printflag=False)
+    solutions, values = genetic_algorithm(N, K, steps, input_type, p_m, objective_function=my_function, printflag=False)
     end = time.time()
-    result_outpout.delete(0.0,END)
-    msg = 'Ο Γεννετικός Αλγόριθμος Ολοκληρώθηκε! Συνολικός χρόνος: ' + str(end-start) + ' sec'
-    result_outpout.insert(END,msg)
+    result_outpout.delete(0.0,tk.END)
+    msg = 'Ο Γεννετικός Αλγόριθμος Ολοκληρώθηκε! Συνολικός χρόνος: ' + '{:.2f}'.format(end-start) + ' sec'
+    result_outpout.insert(tk.END,msg)
     
     #Τύπωσε τις 20 πρώτες λύσεις
     n = 0
@@ -303,9 +330,21 @@ def  run_genetic():
     for s in solutions:
          msg += 'x = '+'{:.4f}'.format(s[0]) + '\t\t' + 'y = ' +'{:.4f}'.format(s[1]) + '\t\t' + 'z = ' + '{:.4f}'.format(s[2]) +   '\t\t value='  +  '{:.2f}'.format(solutions[s]) + '\n'
          n = n + 1 
-         if n>19:
-             break
-    solutions_outpout.insert(END, msg)
+         # if n>19:
+         #     break
+    solutions_outpout.insert(tk.END, msg)
+    
+    #clear canvas
+    # for item in canvas.get_tk_widget().find_all():
+    #    canvas.get_tk_widget().delete(item)
+    #Καθαρίζω το γράφημα ώστε να μπει το καινούριο
+    ax.clear()
+    #draw figure
+    #Σχεδιάζω το καινούριο γράφημα
+    ax.plot(values)
+    #Εμφανίζεται στην οθόνη ο canvas
+    canvas.draw()
+
 
 
 
@@ -328,9 +367,9 @@ def close_window():
 
 #initial values
 #N: Πλήθος αρχικών λύσεων (x, y, z) που θα ξεκινήσουν τον αλγόριθμο
-N = 5000
+N = 500
 #Κ: Πλήθος λύσεων που κρατάει κάθε γενιά (κρατώ τις Κ καλύτερες)
-K = 1000
+K = 100
 #steps: Βήματα που θα κάνει ο γεννετικός αλγόριθμος
 steps = 100
 #input_type:  0 για ακέραιους,  1 για πραγματικούς
@@ -340,45 +379,69 @@ p_m = 0.4
 #printflag
 printflag = False
 
-window = Tk()
+window = tk.Tk()
 window.title("Genetic Algorithm Project")
 #labels
-N_label = Label(window, text="Πλήθος Αρχικών Λύσεων: ", fg="black", font ="none 14 bold" ) .grid(row=0,  column=0, sticky=W)
-K_label = Label(window, text="Πλήθος λύσεων επόμενης γενιάς: ", fg="black", font ="none 14 bold" ) .grid(row=1,  column=0, sticky=W)
-steps_label = Label(window, text="Βήματα: ", fg="black", font ="none 14 bold" ) .grid(row=2,  column=0, sticky=W)
-type_label = N_label = Label(window, text="Τύπος Λύσεων (0: ακέραιοι, 1: πραγματικοί): ", fg="black", font ="none 14 bold" ) .grid(row=3,  column=0, sticky=W)
-pm_label =  Label(window, text="Πιθανότητα Μετάλλαξης: ", fg="black", font ="none 14 bold" ) .grid(row=4,  column=0, sticky=W)
-
+N_label = tk.Label(window, text="Πλήθος Αρχικών Λύσεων: ", fg="black", font ="none 14 bold" ) .grid(row=0,  column=0, sticky=tk.W)
+K_label = tk.Label(window, text="Πλήθος λύσεων επόμενης γενιάς: ", fg="black", font ="none 14 bold" ) .grid(row=1,  column=0, sticky=tk.W)
+steps_label = tk.Label(window, text="Βήματα: ", fg="black", font ="none 14 bold" ) .grid(row=2,  column=0, sticky=tk.W)
+type_label = tk.Label(window, text="Τύπος Λύσεων (0: ακέραιοι, 1: πραγματικοί): ", fg="black", font ="none 14 bold" ) .grid(row=3,  column=0, sticky=tk.W)
+pm_label =  tk.Label(window, text="Πιθανότητα Μετάλλαξης: ", fg="black", font ="none 14 bold" ) .grid(row=4,  column=0, sticky=tk.W)
+function_label = tk.Label(window, text="Συνάρτηση: ", fg="black", font ="none 14 bold" ) .grid(row=5,  column=0, sticky=tk.W)
 
 
 #text entries
-N_entry = Entry(window, width=6, bg="white", font="none 14")
-N_entry.grid(row=0, column=1, sticky=W)
-N_entry.insert(END, N)  #Βάζω αρχική τιμή
-K_entry = Entry(window, width=6, bg="white", font="none 14")
-K_entry.grid(row=1, column=1, sticky=W)
-K_entry.insert(END, K)
-steps_entry = Entry(window, width=6, bg="white", font="none 14")
-steps_entry.grid(row=2, column=1, sticky=W)
-steps_entry.insert(END, steps)
-type_entry = Entry(window, width=3, bg="white", font="none 14")
-type_entry.grid(row=3, column=1, sticky=W)
-type_entry.insert(END, input_type)
-pm_entry = Entry(window, width=6, bg="white", font="none 14")
-pm_entry.grid(row=4, column=1, sticky=W)
-pm_entry.insert(END, p_m)
+N_entry = tk.Entry(window, width=6, bg="white", font="none 14")
+N_entry.grid(row=0, column=1, sticky=tk.W)
+N_entry.insert(tk.END, N)  #Βάζω αρχική τιμή
+K_entry = tk.Entry(window, width=6, bg="white", font="none 14")
+K_entry.grid(row=1, column=1, sticky=tk.W)
+K_entry.insert(tk.END, K)
+steps_entry = tk.Entry(window, width=6, bg="white", font="none 14")
+steps_entry.grid(row=2, column=1, sticky=tk.W)
+steps_entry.insert(tk.END, steps)
+type_entry = tk.Entry(window, width=3, bg="white", font="none 14")
+type_entry.grid(row=3, column=1, sticky=tk.W)
+type_entry.insert(tk.END, input_type)
+pm_entry = tk.Entry(window, width=6, bg="white", font="none 14")
+pm_entry.grid(row=4, column=1, sticky=tk.W)
+pm_entry.insert(tk.END, p_m)
+
+#Dropdown menus
+options_list = ['Standard', 'Function1', 'Function2']
+variable = tk.StringVar(window)
+variable.set(options_list[0]) # default value
+dd_menu = tk.OptionMenu(window, variable, *options_list).grid(row=5, column=1, sticky=tk.W)
+
 
 #Buttons
-run_button = Button(window, text="RUN", width=10, font ="none 18 bold", command=run_genetic).grid(row = 15,  column = 0, sticky=W)
-exit_button = Button(window, text="EXIT", width=10, font ="none 18 bold", command=close_window).grid(row = 15,  column = 1, sticky=E)
+run_button = tk.Button(window, text="RUN", width=10, font ="none 18 bold", command=run_genetic).grid(row = 15,  column = 0, sticky=tk.W)
+exit_button = tk.Button(window, text="EXIT", width=10, font ="none 18 bold", command=close_window).grid(row = 15,  column = 2, sticky=tk.E)
 
 #Outputs!
-result_outpout = Text(window, width=100, height=1, wrap=WORD, background="white")
-result_outpout.grid(row = 20, column=0, columnspan=2, sticky=W)
-space_outpout = Text(window, width=100, height=1, wrap=WORD, background="white")
-space_outpout.grid(row = 21, column=0, columnspan=2, sticky=W)
-solutions_outpout = Text(window, width=100, height=20, wrap=WORD, background="white")
-solutions_outpout.grid(row = 22, column=0, columnspan=2, sticky=W)
+result_outpout = tk.Text(window, width=70, height=1, wrap=tk.WORD, background="white")
+result_outpout.grid(row = 20, column=0, columnspan=2, sticky=tk.W)
+#space_outpout = tk.Text(window, width=80, height=1, wrap=tk.WORD, background="white")
+#space_outpout.grid(row = 21, column=0, columnspan=2, sticky=tk.W)
+solutions_outpout = tk.Text(window, width=70, height=20, wrap=tk.WORD, background="white")
+solutions_outpout.grid(row = 22, column=0, columnspan=2, sticky=tk.W)
+
+
+fig = plt.Figure(figsize=(4.5, 3), dpi=100)
+#Μικραίνω τη γραμματοσειρά του γραφήματος
+plt.rcParams.update({'font.size': 8})
+#Φτιάχνω ένα γράφημα
+ax = fig.add_subplot(111)
+
+canvas = FigureCanvasTkAgg(fig,  master=window)
+canvas.draw()
+canvas.get_tk_widget().grid(row=22, column=2, sticky=tk.E, ipadx=40, ipady=20)
+
+# navigation toolbar
+toolbarFrame = tk.Frame(master=window)
+toolbarFrame.grid(row=23,column=2)
+toolbar = NavigationToolbar2Tk(canvas, toolbarFrame)
+
 
 #Εδώ τρέχει ο κώδικας για το παράθυρο
 window.mainloop()
